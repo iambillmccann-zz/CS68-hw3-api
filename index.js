3338be73c530bd5d1206b2b358f5952d27fe5585
iambillmccann <bill.mccann@gmail.com>
2022-03-02 06:30:13 -0500

    Fix 500 server error when there is no parameter

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 const Cryptr = require('cryptr');
+const { response } = require('express');
 const app = require('express')();
 
 const CRYPTR = new Cryptr('cs 864 homework 3 api');
 const PORT = 8080;
 
+// Setup CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
 });
 
+// Start the API
 app.listen(
     PORT,
     () => console.log(`The server is running on port ${PORT}`)
 );
 
+// Handle the /hw3 endpoint
 app.get('/hw3', (request, response) => {
-    const teamName = request.query.team
-    const encryptedString = CRYPTR.encrypt(teamName)
-    console.log(`Team ${teamName} has the string ${encryptedString}`)
-    response.status(200).send({
+
+    let teamName = '';
+    let encryptedString = '';
+    let statusCode = 200
+
+    if (!request.query.hasOwnProperty('team'))
+        {
+            teamName = 'Team name missing in API call.'
+            statusCode = 400;
+        }
+    else
+        { 
+            teamName = request.query.team;
+            encryptedString = CRYPTR.encrypt(teamName);
+        }
+
+    response.status(statusCode).send({
         team: teamName,
         token: encryptedString
     })
